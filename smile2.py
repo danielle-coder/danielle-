76e2f7a323baa72c99fe5bab799208e10610ccad
danielle-coder <54039873+danielle-coder@users.noreply.github.com>
2019-08-12 14:31:08 -0400

    Add files via upload

---
+++ smile2.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 12 11:25:44 2019
+
+@author: Columbia Girls
+"""
+
+print("hello world")  #gettingtoknowyou
+print("Danielle Dillon")
+5*2
+5/2
+5-2
+5**2
+8/9+3
+8/9 *3
